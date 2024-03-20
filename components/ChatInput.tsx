b364f684b13fe7d3e5f0445e520a034db252754d
simplysabir <simplysabir@gmail.com>
2024-03-20 16:50:07 +0530

    added message menu

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -2,30 +2,37 @@
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import { Input } from "./ui/input";
 import { toast } from "sonner";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { useUser } from "@/lib/store/user";
+import { Imessage, useMessage } from "@/lib/store/messages";
 
 const ChatInput = () => {
   const user = useUser((state) => state.user);
+  const addMessage = useMessage((state) => state.addMessage);
   const supabase = supabaseBrowser();
   const handleSendMessage = async (text: string) => {
-    const newMessage = {
-      id: uuidv4(),
-      text,
-      send_by: user?.id,
-      is_edit: false,
-      created_at: new Date().toISOString(),
-      users: {
-        id: user?.id,
-        display_name: user?.user_metadata.user_name,
-        avatar_url: user?.user_metadata.avatar_url,
+    if (text.trim()) {
+      const newMessage = {
+        id: uuidv4(),
+        text,
+        send_by: user?.id,
+        is_edit: false,
         created_at: new Date().toISOString(),
-      }
-    };
-    const {error} = await supabase.from("messages").insert({text});
-    if (error) {
+        users: {
+          id: user?.id,
+          display_name: user?.user_metadata.user_name,
+          avatar_url: user?.user_metadata.avatar_url,
+          created_at: new Date().toISOString(),
+        },
+      };
+      addMessage(newMessage as Imessage);
+      const { error } = await supabase.from("messages").insert({ text });
+      if (error) {
         toast.error(error.message);
-    }   
+      }
+    } else {
+      toast.error("message can't be empty");
+    }
   };
   return (
     <div className="p-5">
--- components/Message.tsx
+++ components/Message.tsx
@@ -1,25 +1,39 @@
 import { Imessage } from "@/lib/store/messages";
 import React from "react";
 import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { useUser } from "@/lib/store/user";
 
 const Message = ({ message }: { message: Imessage }) => {
+  const user = useUser((state) => state.user);
   return (
     <div className="flex gap-2">
-        <div>
-      <Image
-        src={message.users?.avatar_url!}
-        width={50}
-        height={50}
-        alt={message.users?.display_name!}
-        className="rounded-full ring-2"
-      />
+      <div>
+        <Image
+          src={message.users?.avatar_url!}
+          width={50}
+          height={50}
+          alt={message.users?.display_name!}
+          className="rounded-full ring-2"
+        />
       </div>
       <div className="flex-1">
-        <div className="flex items-center gap-1">
-          <h1 className="font-bold">{message.users?.display_name}</h1>
-          <h1 className="text-sm text-gray-400">
-            {new Date(message.created_at).toDateString()}
-          </h1>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-1">
+            <h1 className="font-bold">{message.users?.display_name}</h1>
+            <h1 className="text-sm text-gray-400">
+              {new Date(message.created_at).toDateString()}
+            </h1>
+          </div>
+          {message.users?.id === user?.id && <MessageMenu />}
         </div>
         <p className="text-gray-300">{message.text}</p>
       </div>
@@ -28,3 +42,19 @@ const Message = ({ message }: { message: Imessage }) => {
 };
 
 export default Message;
+
+const MessageMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <MoreHorizontal />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Action</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Edit</DropdownMenuItem>
+        <DropdownMenuItem>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
