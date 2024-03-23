7047a63e76cedd520fd9a943fdf25f6b9abf30a7
simplysabir <simplysabir@gmail.com>
2024-03-23 17:14:46 +0530

    presence added

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -4,6 +4,7 @@ import { supabaseBrowser } from "@/lib/supabase/browser";
 import { Button } from "./ui/button";
 import { User } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
+import ChatPresence from "./ChatPresence";
 
 const ChatHeader = ({ user }: { user: User | undefined }) => {
   const router = useRouter();
@@ -27,10 +28,7 @@ const ChatHeader = ({ user }: { user: User | undefined }) => {
       <div className="p-5 border-b flex items-center justify-between h-full">
         <div>
           <h1 className="text-xl font-bold">Daily Chat</h1>
-          <div className="flex items-center gap-1">
-            <div className="h-4 w-4 bg-green-500 rounded-full animate-pulse"></div>
-            <h1 className="text-sm text-gray-400">2 onlines</h1>
-          </div>
+          <ChatPresence />
         </div>
         {user ? (
           <Button onClick={handleLogout}>Logout</Button>
